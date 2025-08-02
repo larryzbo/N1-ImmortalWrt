914377af1bec4eb45849ce7f7f47230b8be2170d
Winnie <91746498+Winnie-0721@users.noreply.github.com>
2025-08-02 22:00:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Add packages
-rm -rf package/luci-app-amlogic
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 
 # Update packages
-cp -rf clone/amlogic/luci-app-amlogic 
+cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
 
 # Clean packages
 rm -rf clone
