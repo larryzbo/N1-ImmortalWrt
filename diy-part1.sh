6875656d5573be3f1bf186e6fe33b6a307b75a5d
Winnie <91746498+Winnie-0721@users.noreply.github.com>
2025-08-03 21:00:32 +0800

    Delete diy-part1.sh

--- diy-part1.sh
+++
@@ -1,6 +0,0 @@
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki' >>feeds.conf.default
-echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic.git' >>feeds.conf.default
