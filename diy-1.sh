9b6930817654c076bb1a3fce21f883f6a472b7b5
Winnie <91746498+Winnie-0721@users.noreply.github.com>
2025-08-05 20:49:18 +0800

    Update diy-1.sh

--- diy-1.sh
+++ diy-1.sh
@@ -3,7 +3,7 @@ echo "========================="
 #!/bin/bash
 
 # 添加源
-echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> openwrt/feeds.conf.default
+echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
 
 echo "========================="
 echo " DIY1 配置完成……"
