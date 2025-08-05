cac131eeca1dfb7258d0992389e8548eeff837cd
Winnie <91746498+Winnie-0721@users.noreply.github.com>
2025-08-05 20:29:50 +0800

    Create diy-2.sh

---
+++ diy-2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add packages
+git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+
+# Update packages
+cp -rf clone/amlogic/luci-app-amlogic  feeds/luci/applications/
+
+# Clean packages
+rm -rf clone
