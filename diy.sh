7a4e182f58c1946cf17f1f4f61230351bc1a451b
Winnie <91746498+Winnie-0721@users.noreply.github.com>
2025-08-05 15:32:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # 添加源
-echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> OpenWrt/feeds.conf.default
-echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic.git;main' >> OpenWrt/feeds.conf.default
+echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> openwrt/feeds.conf.default
+echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic.git;main' >> openwrt/feeds.conf.default
