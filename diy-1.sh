ed8f7dbf30902dc2fafd95ee11f427e1ec57ed92
larryzbo <327126598@qq.com>
2025-10-07 16:17:45 +0800

    Update diy-1.sh

--- diy-1.sh
+++ diy-1.sh
@@ -3,7 +3,7 @@
 #添加feeds源
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
 echo "src-git openclash https://github.com/vernesong/OpenClash.git;main" >> feeds.conf.default
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
-echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
+#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+#echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
 
 
