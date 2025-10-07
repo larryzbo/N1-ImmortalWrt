ff6dbdb460b2b0415f46c6a687071e57718d1470
larryzbo <327126598@qq.com>
2025-10-07 14:00:32 +0800

    Update diy-1.sh

--- diy-1.sh
+++ diy-1.sh
@@ -2,3 +2,4 @@
 
 #添加feeds源
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
+echo "src-git openclash https://github.com/vernesong/OpenClash.git;main" >> feeds.conf.default
